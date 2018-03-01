c362f270ecc5889e4c05dbb1741cc899578917d7
Kate Schlunz <schlunzk@gmail.com>
2018-03-01 16:31:32 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word(sentence)
+
+  my_array = sentence.split(/ /)
+  new_array = []
+
+  my_array.each do |word|
+    backwards = word.reverse!
+    new_array.push(backwards)
+  end
+    new_array.join(" ")
+end
+
+
+def reverse_each_word(sentence)
+  my_array = sentence.split(/ /)
+
+my_array.collect do |word|
+    backwards = word.reverse!
+  end
+     my_array.join(" ")
+
+end
